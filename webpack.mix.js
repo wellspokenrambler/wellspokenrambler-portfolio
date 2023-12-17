b4cb6a3dea194827430cd4acddf748a11cbaa069
wellspokenrambler <isabellecmay@gmail.com>
2023-12-17 15:21:34 +0000

    made index page the placeholder page too, sick of this

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.vue({
             notify: false,
             server: {
                 baseDir: "dist",
-                index: "placeholder.html"
+                index: "index.html"
             }
         })
         .options({
